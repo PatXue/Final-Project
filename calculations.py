3eccdfb19dd79eb5fd99a607b7cc3a2fe1cc9f5b
PatXue <95881915+PatXue@users.noreply.github.com>
2023-11-23 12:00:23 -0500

    Add position and momentum updating
    Force calculation currently returns placeholder value

--- calculations.py
+++ calculations.py
@@ -1,17 +1,30 @@
 import numpy as np
 
 class Simulation:
-    def __init__(self, k: float, n: int) -> None:
+    def __init__(self, k: float, n: int, m: float, rest_len: float) -> None:
         '''Create instance of Simulation class
         
         Args:
             k (float): spring constant of each chain
             n (int): number of chains
+            m (float): mass of each chain
+            rest_len (float): rest length of each chain
         '''
         
         self.k = k
         self.n = n
+        self.m = m
+        self.rest_len = rest_len
 
-    def step(self) -> None:
+        self.time = 0.0
+
+        self.pos_array = np.ndarray((3,n))
+        self.mom_array = np.ndarray((3,n))
+    def step(self, dt: float) -> None:
         '''Does physics calculations for one timestep.
         Called by visualization.ipynb'''
+
+        self.mom_array += self.calc_force() * dt
+        self.pos_array += (self.mom_array / self.m) * dt
+        
+        self.time += dt
