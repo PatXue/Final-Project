b88d2104b8037ae35989372f0745ed9ab1764528
PatXue <95881915+PatXue@users.noreply.github.com>
2023-11-30 21:40:34 -0500

    Small edits to visualization.py
    Removed some redundant lines and changed initialization of sim to use variables

--- visualization.py
+++ visualization.py
@@ -5,16 +5,15 @@ from calculations import Simulation
 arr_to_vector = lambda A: vector(A[0],A[1],A[2])
 
 n = 10
+m = 1
 rest_len = 1
-sim = Simulation(500, n, 1, 1)
+sim = Simulation(500, n, m, rest_len)
 dt = 1e-3
 
 scene = canvas(center=vector(rest_len/2,0,0))
 pivot = box(pos=vector(0,0,0),size=vector(0.05,.1,.1),color=color.blue)
 
 spheres = [sphere(pos=arr_to_vector(sim.pos_array[:,i]),radius=0.025,color=color.red) for i in range(n)]
-for i in range(n):
-    spheres[i].pos = arr_to_vector(sim.pos_array[:,i])
 
 # springs = [helix(radius=0.01,thickness=.004,coils=10,color=color.green)] * n
 
